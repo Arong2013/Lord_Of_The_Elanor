d9b48473000256d5ff410e8267b646df2ac7cc0b
Arong2013 <111150462+Arong2013@users.noreply.github.com>
2025-03-13 16:35:19 +0900

    수정

--- Assets/Scripts/Field/GridMarcine.cs
+++ Assets/Scripts/Field/GridMarcine.cs
@@ -12,8 +12,6 @@ class GridMarcine
         movementGrid = new MovementGrid(width, height);
     }
     public void SetMovementValue(Vector3Int pos,bool value) => movementGrid.SetValue(pos,value);
-    public bool CanMove(IGridObject gridObject, Vector3Int newPosition) => movementGrid.CanMove(gridObject, newPosition);
-    public void UpdateGridPos(IGridObject gridObject, bool isOccupied) => movementGrid.SetMovableValue(gridObject, isOccupied);
 }
 
 
